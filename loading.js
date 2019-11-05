c3eee29adbced97382e7f51ed81f2ff5dabcf10a
An Yong Mo <aym506@hotmail.co.kr>
2019-11-05 23:14:17 +0900

    Add loading screen

---
+++ loading.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+export default function Loading()
+{
+    return (
+    <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <Text style={styles.text}>Getting the fokin weather</Text>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create(
+{  
+    container: 
+    {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingVertical: 100,
+        paddingHorizontal: 30,
+        backgroundColor: "#FDF6AA"
+    },
+
+    text: 
+    {
+        color: "#2c2c2c",
+        fontSize: 40
+    }
+});
